cc72825d336e82f71d39f34178ec13b04c4a4c92
Ashiqur Rahman <ashikrobi@gmail.com>
2021-02-09 18:36:17 +0600

    Completed without error handling

--- app.js
+++ app.js
@@ -8,7 +8,40 @@ const searchSongs = () => {
 }
 
 const displaySongs = songs => {
-    console.log(songs);
+    const songContainer = document.getElementById('song-container')
+    songContainer.innerHTML = "";
+    songs.forEach(song => {
+        const songDiv = document.createElement('div')
+        songDiv.className = "single-result row align-items-center my-3 p-3";
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls
+            src="${song.preview}">
+            </audio>
+
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+    `
+        songContainer.appendChild(songDiv)
+    })
+}
+
+
+const getLyric = (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+}
+
+
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('lyrics')
+    lyricsDiv.innerText = lyrics;
 }
 
 
